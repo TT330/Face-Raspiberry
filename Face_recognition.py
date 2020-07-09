bff58cea72bf2d969c6cb173718221ba286b41c3
TT330 <2375607603@qq.com>
2020-07-09 10:21:07 +0800

    树莓派人脸识别“

---
+++ Face_recognition.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+import cv2
+
+from face_train import Model
+
+
+class Face_recognition():
+    def __init__(self):
+        with open('contrast_table', 'r') as f:
+            self.contrast_table = json.loads(f.read())
+        self.model = Model()
+        self.model.load_model(file_path='G:\\face\\renlian\\faces.model')
+        # 框住人脸的矩形边框颜色
+        self.color = (0, 255, 0)
+
+        # 捕获指定摄像头的实时视频流
+        self.cap = cv2.VideoCapture(0)
+
+        # 人脸识别分类器本地存储路径
+        self.cascade_path = "haarcascade_frontalface_default.xml"
+
+    def recongition(self):
+        while True:
+            ret, frame = self.cap.read()  # 读取一帧视频
+
+            if ret is True:
+
+                # 图像灰化，降低计算复杂度
+                frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            else:
+                continue
+            # 使用人脸识别分类器，读入分类器
+            cascade = cv2.CascadeClassifier(self.cascade_path)
+
+            # 利用分类器识别出哪个区域为人脸
+            faceRects = cascade.detectMultiScale(frame_gray, scaleFactor=1.2, minNeighbors=3, minSize=(32, 32))
+            if len(faceRects) > 0:
+                for faceRect in faceRects:
+                    x, y, w, h = faceRect
+
+                    # 截取脸部图像提交给模型识别这是谁
+                    image = frame[y - 10: y + h + 10, x - 10: x + w + 10]
+                    probability, name_number = self.model.face_predict(image)
+                    print(name_number)
+                    name = self.contrast_table[str(name_number)]
+
+                    # print('name_number:', name_number)
+                    cv2.rectangle(frame, (x - 10, y - 10), (x + w + 10, y + h + 10), self.color, thickness=2)
+
+                    # 文字提示是谁
+                    cv2.putText(frame, name, (x + 30, y + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+                    # if probability > 0.7:
+                    #     cv2.putText(frame, name, (x + 30, y + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+                    # else:
+                    #     cv2.putText(frame, 'unknow', (x + 30, y + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+
+            cv2.imshow("face_recognition", frame)
+
+            # 等待10毫秒看是否有按键输入
+            k = cv2.waitKey(10)
+            # 如果输入q则退出循环
+            if k & 0xFF == 27:
+                break
+
+        # 释放摄像头并销毁所有窗口
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+
+
+if __name__ == '__main__':
+    fr = Face_recognition()
+    fr.recongition()
+
---
+++ load_data.py
@@ -0,0 +1,120 @@
+import json
+import os
+
+import cv2
+import numpy as np
+
+IMAGE_SIZE = 64
+
+
+# 按照指定图像大小调整尺寸
+def resize_image(image, height=IMAGE_SIZE, width=IMAGE_SIZE):
+    top, bottom, left, right = (0, 0, 0, 0)
+
+    # 获取图像尺寸
+    h, w, _ = image.shape  # (237, 237, 3)
+
+    # 对于长宽不相等的图片，找到最长的一边
+    longest_edge = max(h, w)
+
+    # 计算短边需要增加多上像素宽度使其与长边等长
+    if h < longest_edge:
+        dh = longest_edge - h
+        top = dh // 2
+        bottom = dh - top
+    elif w < longest_edge:
+        dw = longest_edge - w
+        left = dw // 2
+        right = dw - left
+    else:
+        pass
+
+        # RGB颜色
+    BLACK = [0, 0, 0]
+
+    # 边缘填充 0 给图像增加边界，是图片长、宽等长，cv2.BORDER_CONSTANT指定边界颜色由value指定
+    constant = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=BLACK)
+
+    # 调整图像大小并返回
+    return cv2.resize(constant, (height, width))
+
+
+# 读取训练数据
+images = []
+labels = []
+
+
+def read_path(path_name):
+    for dir_item in os.listdir(path_name):
+
+        # 从初始路径开始叠加，合并成可识别的操作路径
+        full_path = os.path.abspath(os.path.join(path_name, dir_item))
+
+        if os.path.isdir(full_path):  # 如果是文件夹，继续递归调用
+            read_path(full_path)
+        else:  # 文件
+            if dir_item.endswith('.jpg'):
+                image = cv2.imread(full_path)
+                image = resize_image(image, IMAGE_SIZE, IMAGE_SIZE)
+
+                # 放开这个代码，可以看到resize_image()函数的实际调用效果
+                # cv2.imwrite('1.jpg', image)
+
+                images.append(image)
+                labels.append(path_name.split('\\')[-1])
+
+    return images, labels
+
+
+# 从指定路径读取训练数据
+def load_dataset(path_name):
+    images, labels = read_path(path_name)
+    print('labels:', labels)
+
+    # 将输入的所有图片转成四维数组，尺寸为(图片数量*IMAGE_SIZE*IMAGE_SIZE*3)
+    # 我和室友两个人共600张图片，IMAGE_SIZE为64，故对我来说尺寸为1200 * 64 * 64 * 3
+    # 图片为64 * 64像素,一个像素3个颜色值(RGB)
+    images = np.array(images)
+    print(images.shape)
+
+    # 标注数据，'liuzhiyu'文件夹下都是我的脸部图像，全部指定为0，另外一个文件夹下是同学的，全部指定为1
+    labels1 = list(set(labels))
+    face_num = len(labels1)
+    print('face_num:',face_num)
+    num = [i for i in range(face_num)]
+    contrast_table = dict(zip(num, labels1))
+    with open('contrast_table', 'w') as f:
+        f.write(json.dumps(contrast_table))
+    # print('contrast_table:', contrast_table)
+    for index, name in contrast_table.items():
+        for i in range(len(labels)):
+            if labels[i] == name:
+                labels[i] = index
+    # print(labels)
+    labels = np.array(labels)
+
+    return images, labels, face_num
+
+
+# def load_dataset(path_name):
+#     images, labels = read_path(path_name)
+#     print('labels_name:', labels)
+#
+#     # 将输入的所有图片转成四维数组，尺寸为(图片数量*IMAGE_SIZE*IMAGE_SIZE*3)
+#     # 我和室友两个人共600张图片，IMAGE_SIZE为64，故对我来说尺寸为1200 * 64 * 64 * 3
+#     # 图片为64 * 64像素,一个像素3个颜色值(RGB)
+#     images = np.array(images)
+#
+#     # 标注数据，'liuzhiyu'文件夹下都是我的脸部图像，全部指定为0，另外一个文件夹下是同学的，全部指定为1
+#     contrast_table = {"0": "ljw", "1": "lzy", "2": "yzz", "3": "ldh", '4': 'wyz'}
+#     for index, name in contrast_table.items():
+#         for i in range(len(labels)):
+#             if labels[i] == name:
+#                 labels[i] = index
+#     print('labels_number:', labels)
+#     labels = np.array(labels)
+#     return images, labels
+
+
+if __name__ == '__main__':
+    images, labels = load_dataset('G:\\face\\renlian\\faces')
